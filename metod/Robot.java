52ca00f56de32101ea0215a5dad8cc7371a34571
reyhanuneder <50536358+reyhanuneder@users.noreply.github.com>
2019-05-18 14:07:08 +0300

    java2

---
+++ metod/Robot.java
@@ -0,0 +1,21 @@
+
+public class Robot {
+
+	String isim;
+	String renk;
+	int boy;
+
+	void konus() {
+		System.out.println("bugün hava çok güzel");
+	}
+
+	void adimat() {
+
+	}
+
+	void tanit() {
+		System.out.println(isim + "isimli" + renk + "renkli" + boy + "uzunluðunda bir robotum");
+
+	}
+
+}
